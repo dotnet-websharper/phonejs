1c12b1daaabde6cf2f719c7dd3a892ff40a4379d
Jand42 <andras.janko@gmail.com>
2017-09-05 21:55:09 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.cmd
+++ build-zafir.cmd
@@ -2,27 +2,27 @@
 setlocal
 set EnableNuGetPackageRestore=true
 if exist packages del /s /q packages
-tools\NuGet\NuGet install Zafir -pre -o packages -excludeVersion -nocache
-tools\NuGet\NuGet install Zafir.TypeScript -pre -o packages -excludeVersion -nocache
-tools\NuGet\NuGet install Zafir.TypeScript.Lib -pre -o packages -excludeVersion -nocache
-tools\NuGet\NuGet install Zafir.Knockout -pre -o packages -excludeVersion -nocache
+tools\NuGet\NuGet install WebSharper -pre -o packages -excludeVersion -nocache
+tools\NuGet\NuGet install WebSharper.TypeScript -pre -o packages -excludeVersion -nocache
+tools\NuGet\NuGet install WebSharper.TypeScript.Lib -pre -o packages -excludeVersion -nocache
+tools\NuGet\NuGet install WebSharper.Knockout -pre -o packages -excludeVersion -nocache
 tools\NuGet\NuGet install IntelliFactory.Build -pre -o packages -excludeVersion -nocache
 tools\NuGet\NuGet install FSharp.Compiler.Tools -version 4.1.17 -o packages -excludeVersion -nocache
 
 packages\FSharp.Compiler.Tools\tools\fsi.exe --exec tools/configure-zafir.fsx
 
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\FSharp.Core.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\WebSharper.Core.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\WebSharper.TypeScript.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\WebSharper.Core.JavaScript.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\WebSharper.Compiler.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\WebSharper.JQuery.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\Mono.Cecil.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\Mono.Cecil.Pdb.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\Mono.Cecil.Mdb.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\FParsec.dll tools
-xcopy /y /q packages\Zafir.TypeScript\tools\net40\FParsecCS.dll tools
-xcopy /y /q packages\Zafir.Knockout\lib\net40\WebSharper.Knockout.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\FSharp.Core.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\WebSharper.Core.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\WebSharper.TypeScript.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\WebSharper.Core.JavaScript.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\WebSharper.Compiler.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\WebSharper.JQuery.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\Mono.Cecil.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\Mono.Cecil.Pdb.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\Mono.Cecil.Mdb.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\FParsec.dll tools
+xcopy /y /q packages\WebSharper.TypeScript\tools\net40\FParsecCS.dll tools
+xcopy /y /q packages\WebSharper.Knockout\lib\net40\WebSharper.Knockout.dll tools
 xcopy /y /q packages\NuGet.Core\lib\net40-client\NuGet.Core.dll tools
 xcopy /y /q packages\IntelliFactory.Core\lib\net45\IntelliFactory.Core.dll tools
 xcopy /y /q packages\IntelliFactory.Build\lib\net45\IntelliFactory.Build.dll tools
